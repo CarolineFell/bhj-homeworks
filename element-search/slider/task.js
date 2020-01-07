e8ef850cf6c60605804f168b7855a533f6f26ec7
Gradt <carolinefell@yandex.ru>
2020-01-07 17:32:11 +0300

    Combined next-prev-dot in one function

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,62 +1,51 @@
 'use strict';
 
-const sliderArrowPrev = document.querySelector('.slider__arrow_prev');
-const sliderArrowNext = document.querySelector('.slider__arrow_next');
-const sliderItem = document.querySelectorAll('.slider__item');
+const sliderArrowPrev = document.querySelectorAll('.slider__arrow_prev');
+const sliderArrowNext = document.querySelectorAll('.slider__arrow_next');
+const sliderArrows = document.querySelector('.slider__arrows');
 const sliderDot = document.querySelectorAll('.slider__dot');
+const sliderItem = document.getElementsByClassName('slider__item');
+const sliderItemArray = Array.from(sliderItem);
 
-function clickPrev() {
-    for (let i = sliderItem.length - 1; i < sliderItem.length; i--) {  // countdown 
-        if (sliderItem.item(i).classList.contains('slider__item_active')) {     
-            sliderItem.item(i).classList.remove('slider__item_active');
-            sliderDot.item(i).classList.remove('slider__dot_active');
+let currentSlide = 0;
 
-            if (i == 0) {
-                i = sliderItem.length - 1;
-            } else {
-                i -= 1;
-            }
+sliderArrows.onclick = function(event) {
 
-            sliderItem.item(i).classList.add('slider__item_active');
-            sliderDot.item(i).classList.add('slider__dot_active');
-            return;
-        }
+	if (event.target === sliderArrowNext.item(0) && currentSlide === sliderItemArray.length - 1) {
+        sliderItem[sliderItemArray.length - 1].classList.toggle('slider__item_active');
+        sliderDot[sliderItemArray.length - 1].classList.toggle('slider__dot_active');
+        currentSlide = 0;
+        
+	} else if (event.target === sliderArrowNext.item(0)) {
+		currentSlide++;
+        sliderItem[currentSlide - 1].classList.toggle('slider__item_active');
+        sliderDot[currentSlide - 1].classList.toggle('slider__dot_active');
+
+	} else if (event.target === sliderArrowPrev.item(0) && currentSlide === 0) {
+        sliderItem.item(0).classList.toggle('slider__item_active');
+        currentSlide = sliderItemArray.length - 1;
+        
+	} else if (event.target === sliderArrowPrev.item(0)) {
+		currentSlide--;
+        sliderItem[currentSlide + 1].classList.toggle('slider__item_active');
+        sliderDot[currentSlide + 1].classList.toggle('slider__dot_active');
     }
-}
 
-function clickNext() {
-    
-    for (let i = 0; i < sliderItem.length; i++) { // countup
-        if (sliderItem.item(i).classList.contains('slider__item_active')) {     
-            sliderItem.item(i).classList.remove('slider__item_active');
-            sliderDot.item(i).classList.remove('slider__dot_active');
-                
-            if (i == sliderItem.length - 1) {
-                i = 0;
-            } else {
-                i += 1;
+    sliderItem[currentSlide].classList.toggle('slider__item_active');
+    sliderDot[currentSlide].classList.toggle('slider__dot_active');
+
+
+    for (let i=0; i < sliderDot.length; i++) {
+        sliderDot.item(i).onclick = function clickDot() {
+           
+            for (let i=0; i < sliderDot.length; i++) {
+                if (sliderItem.item(i).classList.contains('slider__item_active')) {
+                    sliderDot.item(i).classList.remove('slider__dot_active');
+                    sliderItem.item(i).classList.remove('slider__item_active');
+                }
             }
-
-            sliderItem.item(i).classList.add('slider__item_active');
             sliderDot.item(i).classList.add('slider__dot_active');
-            return;
+            sliderItem.item(i).classList.add('slider__item_active');
         }
-    }
-}
-
-sliderArrowPrev.addEventListener('click', clickPrev);
-sliderArrowNext.addEventListener('click', clickNext);
-
-for (let i=0; i < sliderDot.length; i++) {
-    sliderDot.item(i).onclick = function clickDot() {
-       
-        for (let i=0; i < sliderDot.length; i++) {
-            if (sliderItem.item(i).classList.contains('slider__item_active')) {
-                sliderDot.item(i).classList.remove('slider__dot_active');
-                sliderItem.item(i).classList.remove('slider__item_active');
-            }
-        }
-        sliderDot.item(i).classList.add('slider__dot_active');
-        sliderItem.item(i).classList.add('slider__item_active');
     }
 }
